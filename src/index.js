1692068ee33564caecf6c6ffb6cc46bbc2f4849e
mathvaillant <matheusvaillant505@gmail.com>
2021-02-15 17:05:20 +0000

    first commit

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { sayHi } from './sayHi';
+import './styles/main.scss'
+
+document.querySelector('h1').textContent = sayHi('Hello');
---
+++ src/sayHi.js
@@ -0,0 +1,6 @@
+const sayHi = (greeting) => {
+  console.log('Parcel is running ')
+  return `${greeting}, Stranger!`
+}
+
+export { sayHi } 
